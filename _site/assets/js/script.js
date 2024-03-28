3ed86d40c56dcd07b53d4902f4b83ea336751926
Tejjas Kaul <tejjaskaul@Tejjass-MacBook-Pro.local>
2024-03-28 11:10:47 -0400

    Commit message

--- _site/assets/js/script.js
+++ _site/assets/js/script.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const accordionTitles = document.querySelectorAll('.accordion-title[data-toggle="accordion-content"]');
-
-  accordionTitles.forEach(title => {
-    title.addEventListener('click', function() {
-      const content = this.nextElementSibling;
-      content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    const accordionTitles = document.querySelectorAll('.accordion-title[data-toggle="accordion-content"]');
+  
+    accordionTitles.forEach(title => {
+      title.addEventListener('click', function() {
+        const content = this.nextElementSibling;
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+      });
     });
   });
-});
-
   
